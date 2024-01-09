4cf710706e527e2d9e3b7aa87bd708f0e7abe8e7
mabledevassy <mabledevassyt@gmail.com>
2024-01-09 21:34:59 +0530

    hi

--- src/components/Subcategory/Subcategorydetails.jsx
+++ src/components/Subcategory/Subcategorydetails.jsx
@@ -1,17 +1,26 @@
 import { Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@mui/material'
 import axios from 'axios';
 import React, { useEffect, useState } from 'react'
+import EditIcon from '@mui/icons-material/Edit';
+import Subcategoryedit from './Subcategoryedit';
 
 const Subcategorydetails = () => {
     var[sub,setSub]=useState([]);
+    var [selected,setSelected]=useState();
+  var [update,setUpdate]=useState(false);
+
     useEffect(()=>{
-        axios.get("http://localhost:3005/view")
+        axios.get("http://localhost:3005/views")
         .then(response=>{
             setSub(response.data)
         })
         .catch(err=>console.log(err))
     },[])
-  return (
+    const updateValues = (row) => {
+        setSelected(row)
+        setUpdate(true)
+    }
+    var result=
     <div>
       <TableContainer component={Paper}>
             <Table sx={{ minWidth: 650 }} aria-label="simple table">
@@ -31,6 +40,7 @@ const Subcategorydetails = () => {
                                     {row.Sname}
                                 </TableCell>
                                 <TableCell>{row.Category}</TableCell>
+                                <TableCell><EditIcon onClick={()=>updateValues(row)}/></TableCell>
                                
                             </TableRow>
 
@@ -40,7 +50,9 @@ const Subcategorydetails = () => {
             </Table>
         </TableContainer>
     </div>
-  )
+  if(update)
+  result=<Subcategoryedit data={selected} method='put'/>
+  return(result)
 }
 
 export default Subcategorydetails
